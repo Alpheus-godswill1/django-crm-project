91194dc1e14bf04940bf701816a49d1bdbebbc74
Alpheus Godswill <harryalph9@gmail.com>
2024-02-29 20:30:10 +0100

    Added the app level template to 'LeadTracker' app:
    Created the 'templates\LeadTracker' directory in the 'LeadTracker' app .
    Added the route 'LeadTracker/index.html'  to the 'urls.py' file of the 'LeadTracker' app to render the request of the client from the 'views.py' file

--- LeadTracker/views.py
+++ LeadTracker/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'LeadTracker/index.html')    # This is the path to the index.html file 
+
