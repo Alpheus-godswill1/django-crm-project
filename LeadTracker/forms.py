d36f2618d7bb2bb3977282ecd20f084c1b673c59
Alpheus Godswill <harryalph9@gmail.com>
2024-03-01 22:10:53 +0100

    Editted the Forms.py and models.py files:
    In the forms.py file inside the 'LeadTracker' app  I created 2 classes for Register and login of user details, using django libraries

--- LeadTracker/forms.py
+++ LeadTracker/forms.py
@@ -1,2 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
+
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.forms.widgets import PasswordInput, TextInput
+
+from django import forms
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta:
+        
+        models = User
+        field = ['username', 'email', 'password1', 'password2']
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+    password = forms.CharField(label="Password", max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
