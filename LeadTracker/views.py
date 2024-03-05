866137dcd998b59ceee48ccaa351eb3eda3e848e
Alpheus Godswill <harryalph9@gmail.com>
2024-03-05 22:33:56 +0100

    A view called Register  was created in Views.py file in the LeadTracker app

--- LeadTracker/views.py
+++ LeadTracker/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
-
+from .forms import CustomUserCreationForm, LoginForm
 
 def index(request):
     return render(request, 'LeadTracker/index.html')    # This is the path to the index.html file 
+
+def registerPage(request):
+    form = CustomUserCreationForm()
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect('login')
+
 
