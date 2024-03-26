f2885db3fe5db4b0abf73853244a0fbdb5af4030
Alpheus Godswill <harryalph9@gmail.com>
2024-03-26 16:32:30 +0100

    Form Creation:
    I created a more suitable form using the 'crispy_forms' application which installed using python package manager

--- DjangoEliteCRM/settings.py
+++ DjangoEliteCRM/settings.py
@@ -28,7 +28,11 @@ INSTALLED_APPS = [
     
     
     'LeadTracker',
+    
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
