356895d213fd52cf51d00b508eadf3c6ca93b07c
Alpheus Godswill <harryalph9@gmail.com>
2024-03-26 16:34:48 +0100

    LeadTracker/urls.py
    Added the Loging page view to the thist file to enable the easier login navigation of the users

--- LeadTracker/urls.py
+++ LeadTracker/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('register', views.registerPage, name='register'),
+    path('login', views.loginPage, name='login'),
+    
 ]
